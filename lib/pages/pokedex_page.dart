4650958cfbdf2e9636663da8b7b0a5a3387729ff
Sacha <sacha.barbet@gmail.com>
2024-03-08 18:45:48 +0100

    Mise à jour des splash screen pour android 12. Ajout du model pokemon. Ajout de la page Pokedex.

---
+++ lib/pages/pokedex_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../assets/app_colors.dart';
+
+class PokedexPage extends StatelessWidget {
+  const PokedexPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Loading..."),
+      ),
+    );
+  }
+}
