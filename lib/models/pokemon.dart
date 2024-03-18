ba80625648f5807153ca573ff890042e2b219612
barbetsacha <sacha.barbet@gmail.com>
2024-03-18 11:29:23 +0100

    Adaptation du model pokemon

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -1,13 +1,27 @@
 class Pokemon {
   final String name;
   final String imageUrl;
-  final String type;
-  final String description;
+  final String? type;
+  final String? description;
 
   Pokemon({
     required this.name,
     required this.imageUrl,
-    required this.type,
-    required this.description,
+    this.type,
+    this.description,
   });
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) {
+    return Pokemon(
+      name: json['name'],
+      imageUrl: json['sprites']['front_default'],
+      type: json['types'][0]['type']['name'],
+      description: json['species']['url'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Pokemon{name: $name, imageUrl: $imageUrl, type: $type, description: $description}';
+  }
 }
